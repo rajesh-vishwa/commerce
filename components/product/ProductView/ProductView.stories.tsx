6e2651509968198d26c7f8502adf7e5252c78b58
RV <sms.rajesh@gmail.comm>
2020-12-14 23:35:10 +0530

    story removed

--- components/product/ProductView/ProductView.stories.tsx
+++ components/product/ProductView/ProductView.stories.tsx
@@ -4,6 +4,6 @@ import ProductView from './ProductView'
 import { p } from './data'
 
 storiesOf('ProductView', module).add('default', () => {
-  return <ProductView product={p} />
-  //return <div>A</div>
+  //return <ProductView product={p} />
+  return <div>A</div>
 })
