56c61837e1a1150464d4e936958d57b3c0eac3cc
RV <sms.rajesh@gmail.comm>
2020-12-14 19:11:10 +0530

    removed stories

--- components/product/ProductCard/ProductCard.stories.tsx
+++ components/product/ProductCard/ProductCard.stories.tsx
@@ -5,37 +5,40 @@ import { productData } from '../productData'
 
 storiesOf('ProductCard', module)
   .add('variant simple', () => {
-    return (
-      <ProductCard
-        product={productData}
-        variant="simple"
-        imgWidth={480}
-        imgHeight={480}
-        imgPriority
-        imgLoading="eager"
-        imgLayout="fixed"
-      />
-    )
+    // return (
+    //   <ProductCard
+    //     product={productData}
+    //     variant="simple"
+    //     imgWidth={480}
+    //     imgHeight={480}
+    //     imgPriority
+    //     imgLoading="eager"
+    //     imgLayout="fixed"
+    //   />
+    // )
+    return <div>A</div>
   })
   .add('variant slim', () => {
-    return (
-      <ProductCard
-        product={productData}
-        variant="slim"
-        imgWidth={320}
-        imgHeight={320}
-        imgLayout="fixed"
-      />
-    )
+    // return (
+    //   <ProductCard
+    //     product={productData}
+    //     variant="slim"
+    //     imgWidth={320}
+    //     imgHeight={320}
+    //     imgLayout="fixed"
+    //   />
+    // )
+    return <div>A</div>
   })
   .add('animated', () => {
-    return (
-      <ProductCard
-        product={productData}
-        className="animated fadeIn"
-        variant="simple"
-        imgWidth={480}
-        imgHeight={480}
-      />
-    )
+    // return (
+    //   <ProductCard
+    //     product={productData}
+    //     className="animated fadeIn"
+    //     variant="simple"
+    //     imgWidth={480}
+    //     imgHeight={480}
+    //   />
+    // )
+    return <div>A</div>
   })
